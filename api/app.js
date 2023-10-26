4bf2dda34c53cd744364ecaccd41f068f67b996a
Prince Raj <profile.princeraj+github@gmail.com>
2023-10-26 23:10:44 +0530

    finalizing for vercel production

--- api/app.js
+++ api/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Define your API routes
-app.use('/api', require('./routes/profile'));
+app.use('/api', require('../src/routes/profile'));
 
 app.get('/', (req, res) => {
     res.send("Yea, I am here!");
