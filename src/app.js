0c388516f960fa605d74c0eb862acee3c975732b
Prince Raj <profile.princeraj+github@gmail.com>
2023-10-26 21:46:22 +0530

    .

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ const port = process.env.PORT || 3000;
 
 // Define your API routes
 app.use('/api', require('./routes/profile'));
-
+app.use('/direct-api', (req, res) => {
+    res.send('API is running.');
+})
 app.use('/test', (req, res) => {
     res.send("HI!");
 })
