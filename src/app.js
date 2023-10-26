bff7fcb0fba130af2c93b7cc58c6a8ef33997d08
Prince Raj <profile.princeraj+github@gmail.com>
2023-10-26 20:18:54 +0530

    .

--- src/app.js
+++ src/app.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
 const port = process.env.PORT || 3000;
 
-// Read the CORS environment variable
-const allowedOrigin = process.env.CORS || '*';
-
-// Enable CORS for all routes
-app.use(cors({ origin: allowedOrigin }));
+// // Read the CORS environment variable
+// const allowedOrigin = process.env.CORS || '*';
+// // Enable CORS for all routes
+// app.use(cors({ origin: allowedOrigin }));
 
 // Define your API routes
 app.use('/api', require('./routes/profile'));
