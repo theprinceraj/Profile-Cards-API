a9ddc3c48dd9155b4d96cd65d374212a62b55a5e
Prince Raj <profile.princeraj+github@gmail.com>
2023-10-26 22:58:23 +0530

    .

--- src/app.js
+++ src/app.js
@@ -14,6 +14,6 @@ app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-// Test URL: http://localhost:3000/api/profile?name=Prince%20Raj&location=India&socialMedia=Github&socialMediaUsername=theprinceraj&title=Web%20Developer&skills=HTML,CSS,JavaScript&imageLink=https://cdn.discordapp.com/avatars/564327207133249536/2a80d088463a3751c63ebb0b0b64f0e9.png?size=1024
+// Test URL: http://localhost:3000/api/profile?name=Prince%20Raj&location=India&socialMedia=Github&socialMediaUsername=theprinceraj&title=Web%20Developer&skills=HTML,CSS,JavaScript&imageLink=https://cdn.discordapp.com/avatars/564327207133249536/2a80d088463a3751c63ebb0b0b64f0e9.png
 
 module.exports = app;
