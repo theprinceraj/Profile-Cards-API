eb34112c4a8cb6bb1390991321e85d508b8e4c0d
Prince Raj <profile.princeraj+github@gmail.com>
2023-10-27 20:57:01 +0530

    adding @napi-rs/canvas starting code

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -1,42 +1,61 @@
-const puppeteer = require('puppeteer');
-const fs = require('fs');
+const { createCanvas, loadImage, createPNGStream } = require('@napi-rs/canvas');
+const fetch = require('node-fetch');
 
 /**
  * Asynchronously generates a profile card image using Puppeteer.
  * 
  * @return {Buffer} The image buffer of the generated profile card.
  */
-async function generateProfileCard(imageLink, name, location, title, socialMedia, socialMediaUsername, skills) {
+async function generateProfileCard(image, name, location, title, socialMedia, socialMediaUsername, skills) {
     try {
-        const browser = await puppeteer.launch({ headless: "new" });
-        const page = await browser.newPage();
+        // Create a canvas
+        const canvas = createCanvas(350, 464); // Adjust dimensions as needed
+        const context = canvas.getContext('2d');
 
-        // const htmlCode = ;
+        // Draw background
+        context.fillStyle = '#231E39';
+        context.fillRect(0, 0, canvas.width, canvas.height);
 
-        // Replace placeholders in the HTML template with the provided values
-        const customizedHtml = fs.readFileSync('template/index.html', 'utf-8')
-            .replace('{{imageLink}}', imageLink)
-            .replace('{{name}}', name)
-            .replace('{{location}}', location)
-            .replace('{{title}}', title)
-            .replace('{{skills}}', convertSkilsToLiItems(skills || 'None'))
-            .replace('{{socialMedia}}', socialMedia)
-            .replace('{{socialMediaUsername}}', socialMediaUsername)
+        // Create a clipping path to make the image a circle
+        context.beginPath();
+        context.arc(175, 125, 75, 0, Math.PI * 2);
+        context.closePath();
+        context.clip();
 
-        await page.setContent(customizedHtml);
-        await page.addStyleTag({ path: 'template/style.css' });
-        await page.setViewport({ width: 350, height: 450, deviceScaleFactor: 2 });
+        // Draw the image inside the circle
+        context.drawImage(image, 100, 50, 150, 150);
 
-        const imageBuffer = await page.screenshot();
+        // Reset the clipping path
+        context.restore(); // Restore previous canvas state
 
-        await browser.close();
+        // Draw text
+        context.fillStyle = '#B3B8CD';
+        context.font = '19px Montserrat';
+        context.fillText(name, 50, 300);
+        context.font = '11px Montserrat';
+        context.fillText(location, 50, 330); // Adjust the position as needed
+        context.font = '14px Montserrat';
+        context.fillText(title, 50, 360); // Adjust the position as needed
+        context.font = '13px Montserrat';
+        context.fillText(socialMedia, 50, 390); // Adjust the position as needed
+        context.fillText(socialMediaUsername, 50, 420); // Adjust the position as needed
 
-        return imageBuffer;
+        // Draw skills
+        context.fillStyle = '#B3B8CD';
+        context.font = '14px Montserrat';
+        const skillsList = skills.split(',');
+        skillsList.forEach((skill, index) => {
+            context.fillText(skill, 100, 360 + index * 20); // Adjust the position as needed
+        });
+
+        const finalOutput = canvas.encode('jpeg');
+        return finalOutput;
     } catch (error) {
         console.error('Error generating the profile card image:', error);
         return null;
     }
 }
+
 
 
 /**
@@ -49,11 +68,34 @@ async function generateProfileCard(imageLink, name, location, title, socialMedia
 async function getProfileCard(req, res) {
     try {
         const { imageLink, name, location, title, socialMedia, socialMediaUsername, skills } = req.query;
-        const imageBuffer = await generateProfileCard(imageLink, name, location, title, socialMedia, socialMediaUsername, skills);
 
-        if (imageBuffer) {
+        if (!imageLink || !name || !location || !title || !socialMedia || !socialMediaUsername || !skills) {
+            res.status(400).send('Invalid query parameters.');
+            return;
+        }
+
+        const imageResponse = await fetch(imageLink);
+        if (!imageResponse.ok) {
+            res.status(500).send('Failed to fetch the image from the provided URL.');
+            return;
+        }
+
+        const imageBuffer = await imageResponse.buffer();
+        const fetchedImage = await loadImage(imageBuffer);
+
+        const profileCardImageBuffer = await generateProfileCard(
+            fetchedImage,
+            name,
+            location,
+            title,
+            socialMedia,
+            socialMediaUsername,
+            skills
+        );
+
+        if (profileCardImageBuffer) {
             res.set('Content-Type', 'image/png');
-            res.send(imageBuffer);
+            res.send(profileCardImageBuffer);
         } else {
             res.status(500).send('Failed to generate the profile card image.');
         }
@@ -63,12 +105,7 @@ async function getProfileCard(req, res) {
     }
 }
 
-function convertSkilsToLiItems(skillsString) {
-    const skillsArray = skillsString?.split(',') || [];
-    const liItems = skillsArray.map((skill) => `<li>${skill}</li>`);
-    return liItems.join('');
-}
-
+// export getProfileCard function using ES6 syntax
 module.exports = {
-    getProfileCard,
-};
+    getProfileCard
+}
