2d67a980804b72986bc4d2dc3b601ba0dab86415
Prince Raj <116755566+theprinceraj@users.noreply.github.com>
2023-12-14 00:22:54 +0530

    Feature upload image (#12)
    * final touch
    
    * final touch

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -28,7 +28,6 @@ function updateImageSrc() {
             newSrc = `https://profile-cards-api.vercel.app/api/profile?name=${name}&location=${location}&title=${title}&imageLink=${imageLink}`;
         }
         profileImage.src = newSrc;
-        pauseTimer();
         formFilledCorrectly = true;
     } else {
         formFilledCorrectly = false;
