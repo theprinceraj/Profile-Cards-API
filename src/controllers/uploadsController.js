96960ffb5fa108647b39626c5bba6b317359d257
Prince Raj <profile.princeraj+github@gmail.com>
2023-12-14 20:15:05 +0530

    Add logic for uploading images to IMG BB server

--- src/controllers/uploadsController.js
+++ src/controllers/uploadsController.js
@@ -1,3 +1,20 @@
+/**
+ * Uploads a file to the IMG BB server.
+ * 
+ * This function takes in a request object `req` and a response object `res`. It expects the request body to contain an `image` field with the base64 version of the image to be uploaded. If the `image` field is not present or empty, it will send a 400 Bad Request response with the message 'No file uploaded.' If the `image` field is present, it will create a new FormData object, append the `image` field to it, and then send a POST request to the IMG BB API with the FormData as the body. If the request is successful (status code 200), it will parse the response body as JSON and send a JSON response with the uploaded image's URL. If the request is unsuccessful, it will parse the error response body and throw an Error with the message 'Image upload failed'. If an error occurs during the execution of the function, it will catch the error, log it to the console, and send a 500 Internal Server Error response.
+ *
+ * The logic for uploading to the IMG BB server is as follows:
+ * - Create a new FormData object.
+ * - Append the `image` field (base64 version of the image) to the FormData.
+ * - Send a POST request to the IMG BB API with the FormData as the body.
+ * - If the request is successful (status code 200), parse the response body as JSON.
+ * - Send a JSON response with the uploaded image's URL.
+ * - If the request is unsuccessful, parse the error response body and throw an Error with the message 'Image upload failed'.
+ *
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @return {Promise<void>} The function does not return anything.
+ */
 async function uploadFileToIMG_BB_Server(req, res) {
     const base64versionImage = req.body.image;
     if (!base64versionImage) {
