1583b029071d1827077b184915ab71393c56554a
Prince Raj <profile.princeraj+github@gmail.com>
2023-12-16 16:45:20 +0530

    Debounce function no longer returns a promise

--- public/scripts/debounceFunctions.js
+++ public/scripts/debounceFunctions.js
@@ -1,23 +1,9 @@
-/**
- * Returns a debounced version of the given function that will only be called after a specified timeout period has elapsed since the last invocation.
- *
- * @param {Function} fn - The function to be debounced.
- * @param {number} timeout - The amount of time in milliseconds to wait before invoking the debounced function (default is 300ms).
- * @returns {Function} - The debounced function.
- */
 const debounceFnCall = (fn, timeout = 300) => {
     let debounceTimer;
     return async (...args) => {
-        return new Promise((resolve, reject) => {
-            clearTimeout(debounceTimer);
-            debounceTimer = setTimeout(async () => {
-                try {
-                    const result = await fn.apply(this, args);
-                    resolve(result);
-                } catch (err) {
-                    reject(err);
-                }
-            }, timeout);
-        })
+        clearTimeout(debounceTimer);
+        debounceTimer = setTimeout(async () => {
+            await fn.apply(this, args);
+        }, timeout);
     }
 }
