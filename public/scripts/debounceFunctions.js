445aca9e5ce7c843f0832e7e8b608c4b23d965c7
Prince Raj <profile.princeraj+github@gmail.com>
2023-12-17 17:57:19 +0530

    Debounce function for API calls

--- public/scripts/debounceFunctions.js
+++ public/scripts/debounceFunctions.js
@@ -1,7 +1,5 @@
 const debounceFnCall = (fn, timeout = 300) => {
     let debounceTimer;
-    return (...args) => {
-        clearTimeout(debounceTimer);
-        debounceTimer = setTimeout(fn.apply(this, args), timeout);
-    }
+    clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(fn, timeout);
 }
