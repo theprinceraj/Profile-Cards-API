d9ebaeb3c34edd8495b86d2032f84ab74e4f40fa
Prince Raj <profile.princeraj+github@gmail.com>
2023-12-27 15:13:55 +0530

    .

--- public/scripts/shortLinkBtnFunctionality.js
+++ public/scripts/shortLinkBtnFunctionality.js
@@ -1,14 +1,15 @@
 shortenLinkBtn.addEventListener('click', async () => {
-    shortenLinkBtn.textContent = 'Generating...';
-    shortenLinkBtn.classList.toggle('btn-primary');
-    shortenLinkBtn.classList.toggle('btn-secondary');
-
     validateInputFields();
-
     if (!navigator.clipboard) {
         alert('Your browser does not support the Clipboard API');
         return;
     }
+
+    shortenLinkBtn.textContent = 'Generating...';
+    shortenLinkBtn.classList.toggle('btn-primary');
+    shortenLinkBtn.classList.toggle('btn-secondary');
+
+
 
     const res = await fetch(`/api/shorten?longUrl=${encodeURIComponent(profileImage.src)}`);
     const data = await res.json();
