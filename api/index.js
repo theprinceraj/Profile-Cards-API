ddbabbb795e9dc2508fd64669cf679f9ba17be40
Prince Raj <profile.princeraj+github@gmail.com>
2023-12-28 14:24:23 +0530

    .

--- api/index.js
+++ api/index.js
@@ -14,23 +14,6 @@ app.use('/api', apiRoute);
 import shortLinksRoute from '../src/routes/shortLinksRoute.js';
 app.use('/c', shortLinksRoute);
 
-// import { fetchLongUrl } from '../src/utilities/database-functions.js';
-// app.get('/c/:shortId', async (req, res) => {
-//     const shortId_ = req.params.shortId;
-//     if (!shortId_) {
-//         return res.status(400).send('Invalid shortId');
-//     }
-
-//     let baseUrl = 'http://localhost:3000';
-//     if (req) {
-//         baseUrl = `${req.protocol}://${req.get('host')}`;
-//     }
-//     const shortUrl = `${baseUrl}/c/${shortId_}`;
-
-//     const longUrl = await fetchLongUrl(shortUrl);
-//     res.redirect(longUrl);
-// });
-
 // Start the server
 app.listen(port, () => {
     console.log(`****************************`);
