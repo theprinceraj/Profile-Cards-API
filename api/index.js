84965ff9ff41d107f624fd6ad92d16d749f2b53d
theprinceraj <profile.princeraj@gmail.com>
2024-09-26 01:27:08 +0530

    Fixed cors

--- api/index.js
+++ api/index.js
@@ -1,24 +1,23 @@
-import express from 'express';
+import express from "express";
 const app = express();
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 const port = process.env.PORT || 3000;
 
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
-app.use(express.static('public'));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(express.static("public"));
 
-import apiRoute from '../src/routes/apiRoute.js';
-app.use('/api', apiRoute);
+import apiRoute from "../src/routes/apiRoute.js";
+app.use("/api", apiRoute);
 
-import shortLinksRoute from '../src/routes/shortLinksRoute.js';
-app.use('/c', shortLinksRoute);
+import shortLinksRoute from "../src/routes/shortLinksRoute.js";
+app.use("/c", shortLinksRoute);
 
 app.listen(port, () => {
     console.log(`****************************`);
     console.log(`Made with 💓 by Prince Raj!`);
     console.log(`****************************`);
 });
-
 
 export default app;
